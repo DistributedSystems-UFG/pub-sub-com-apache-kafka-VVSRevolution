0229ec9960a68219567f54c8b45354ae7138865c
Vinicio Vieira dos Santos <68493091+VVSRevolution@users.noreply.github.com>
2023-06-30 15:13:56 -0300

    1

--- consumer.py
+++ consumer.py
@@ -2,12 +2,19 @@ from kafka import KafkaConsumer
 from const import *
 import sys
 
-consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
 try:
   topic = sys.argv[1]
+  username = "admin"
+  password = "admin-secret"
 except:
-  print ('Usage: python3 consumer <topic_name>')
+  print ('Usage: python3 consumer <topic_name> <username> <password>')
   exit(1)
+
+consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT],
+                         security_protocol='SASL_PLAINTEXT',
+                         sasl_mechanism='PLAIN',
+                         sasl_plain_username=username,
+                         sasl_plain_password=password)
   
 consumer.subscribe([topic])
 for msg in consumer:
--- producer.py
+++ producer.py
@@ -1,17 +1,29 @@
 from kafka import KafkaProducer
 from const import *
 import sys
+import time
 
 try:
     topic = sys.argv[1]
+    username = "admin"
+    password = "admin-secret"
 except:
-    print ('Usage: python3 producer <topic_name>')
+    print ('Usage: python3 producer <topic_name> <username> <password>')
     exit(1)
     
-producer = KafkaProducer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
-for i in range(100):
-    msg = 'My ' + str(i) + 'st message for topic ' + topic
-    print ('Sending message: ' + msg)
-    producer.send(topic, value=msg.encode())
+producer = KafkaProducer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT],
+                         security_protocol='SASL_PLAINTEXT',
+                         sasl_mechanism='PLAIN',
+                         sasl_plain_username=username,
+                         sasl_plain_password=password)
 
-producer.flush()
+try: 
+    i = 0
+    while True:
+        i = i + 1
+        msg = 'My ' + str(i) + 'st message for topic ' + topic
+        print ('Sending message: ' + msg)
+        producer.send(topic, value=msg.encode())
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    producer.flush()
